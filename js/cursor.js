f8febb5d607d63574b6e99a3ae30e90e194a45a8
lisiqi <374912801@qq.com>
2022-10-09 21:14:38 +0800

    Site updated: 2022-10-09 21:14:37

---
+++ js/cursor.js
@@ -0,0 +1,14 @@
+/*!
+ * Fairy Dust Cursor.js
+ * - 90's cursors collection
+ * -- https://github.com/tholman/90s-cursor-effects
+ * -- http://codepen.io/tholman/full/jWmZxZ/
+ */
+
+(function fairyDustCursor() {
+
+    var possibleColors = ["#D61C59", "#E7D84B", "#1B8798"]
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var cursor = { x: width / 2, y: width / 2 };
+    var particles = [];
